eff24e2d9bbc3208a00b72754c2675eb1853f126
Gio <g.ruscigno@icloud.com>
2025-01-26 16:09:41 -0600

    add dependencies and environment

--- RedPaprika/App Environment/App+EnvironmentValues.swift
+++ RedPaprika/App Environment/App+EnvironmentValues.swift
@@ -1,4 +1,77 @@
 // Red Paprika recipe app
 // Giorgio Ruscigno, 2025.
 
-import Foundation
+import SwiftData
+import SwiftUI
+
+/// Environment key for dependencies
+extension EnvironmentValues {
+    /// This dependencies implementation only exists to provide a default value for the environment.
+    /// It's not meant to be used (thus is private) and the app will crash if the appropriate dependencies are not set.
+    private struct DefaultDependencies: Dependencies {
+        func makeImageService() -> any ImageService {
+            assertionFailure("Dependencies not set. Use the environment to set a proper value.")
+            fatalError("Dependencies not set. Use the environment to set a proper value.")
+        }
+        
+        func makeImageCache(container: ModelContainer) -> any ImageCache {
+            assertionFailure("Dependencies not set. Use the environment to set a proper value.")
+            fatalError("Dependencies not set. Use the environment to set a proper value.")
+        }
+        
+        func makeModelContainer() -> ModelContainer {
+            assertionFailure("Dependencies not set. Use the environment to set a proper value.")
+            fatalError("Dependencies not set. Use the environment to set a proper value.")
+        }
+
+        func makeRecipeStore(container: ModelContainer) -> RecipeStore {
+            assertionFailure("Dependencies not set. Use the environment to set a proper value.")
+            fatalError("Dependencies not set. Use the environment to set a proper value.")
+        }
+
+        @MainActor
+        func makeMainViewModel(store: RecipeStore) -> MainViewModel {
+            assertionFailure("Dependencies not set. Use the environment to set a proper value.")
+            fatalError("Dependencies not set. Use the environment to set a proper value.")
+        }
+    }
+
+    @Entry var dependencies: Dependencies = DefaultDependencies()
+}
+
+/// Environment key for image cache
+extension EnvironmentValues {
+    /// This dependencies implementation only exists to provide a default value for the environment.
+    /// It's not meant to be used (thus is private) and the app will crash if the appropriate dependencies are not set.
+    private struct DefaultImageCache: ImageCache {
+        func getImage(from url: String) async throws -> Image {
+            assertionFailure("Dependencies not set. Use the environment to set a proper value.")
+            fatalError("Dependencies not set. Use the environment to set a proper value.")
+        }
+        
+        func store(from url: String, service: any ImageService) async -> Bool {
+            assertionFailure("Dependencies not set. Use the environment to set a proper value.")
+            fatalError("Dependencies not set. Use the environment to set a proper value.")
+        }
+    }
+
+    @Entry var imageCache: ImageCache = DefaultImageCache()
+}
+
+extension EnvironmentValues {
+    /// This dependencies implementation only exists to provide a default value for the environment.
+    /// It's not meant to be used (thus is private) and the app will crash if the appropriate dependencies are not set.
+    private struct DefaultImageService: ImageService {
+        func fetchImageData(from url: String) async throws -> Data {
+            assertionFailure("Dependencies not set. Use the environment to set a proper value.")
+            fatalError("Dependencies not set. Use the environment to set a proper value.")
+        }
+    }
+    @Entry var imageService: ImageService = DefaultImageService()
+}
+
+/// Environment key for adaptive card width
+extension EnvironmentValues {
+    /// Store the carousel width based on the current `Geometry`
+    @Entry var cardWidth: CGFloat = 300
+}
--- RedPaprika/DI/Dependencies.swift
+++ RedPaprika/DI/Dependencies.swift
@@ -1,4 +1,64 @@
 // Red Paprika recipe app
 // Giorgio Ruscigno, 2025.
 
-import Foundation
+import SwiftData
+
+protocol Dependencies: Sendable {
+    func makeModelContainer() -> ModelContainer
+    func makeRecipeStore(container: ModelContainer) -> RecipeStore
+    func makeImageCache(container: ModelContainer) -> ImageCache
+    func makeImageService() -> ImageService
+    @MainActor
+    func makeMainViewModel(store: RecipeStore) -> MainViewModel
+}
+
+struct AppDependencies: Dependencies {
+    /// Instantiate the shared model container for the app
+    /// - Returns: the newly created container
+    func makeModelContainer() -> ModelContainer {
+        let schema = Schema([
+            Cuisine.self,
+            Recipe.self,
+            CachedImage.self
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }
+
+    /// Instantiate the recipe store
+    /// - Parameter service: the remote service to fetch recipes from
+    /// - Parameter container: model container
+    /// - Returns: the newly created recipe store
+    func makeRecipeStore(container: ModelContainer) -> RecipeStore {
+        SwiftDataRecipeStore(modelContainer: container)
+    }
+    
+    /// Instantiate the image cache
+    /// - Parameter container: model container
+    /// - Returns: the newly created image cache
+    func makeImageCache(container: ModelContainer) -> ImageCache {
+        SwiftDataImageCache(modelContainer: container)
+    }
+
+    func makeImageService() -> ImageService {
+        AppImageService(client: makeClient())
+    }
+
+    @MainActor
+    func makeMainViewModel(store: RecipeStore) -> MainViewModel {
+        MainViewModel(store: store, service: makeRecipeService())
+    }
+
+    private func makeRecipeService() -> RecipeService {
+        AppRecipeService(client: makeClient())
+    }
+
+    private func makeClient() -> Client {
+        HTTPClient()
+    }
+}
