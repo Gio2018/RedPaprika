6d74b5868718dd771990a894a3bba418a817be65
Gio <g.ruscigno@icloud.com>
2025-01-26 16:10:16 -0600

    add view models

--- RedPaprika/ViewModel/ImageRenderer.swift
+++ RedPaprika/ViewModel/ImageRenderer.swift
@@ -2,3 +2,26 @@
 // Giorgio Ruscigno, 2025.
 
 import Foundation
+
+/// Holds the state of a `RemoteImage` instance depending on the state of the cached/loaded image.
+@MainActor
+final class ImageRenderer: ObservableObject {
+    private let cache: ImageCache
+    private let service: ImageService
+    private let url: String
+
+    @Published var state: ViewState = .idle
+
+    init(cache: ImageCache, service: ImageService, url: String) {
+        self.cache = cache
+        self.service = service
+        self.url = url
+    }
+
+    func storeImage() async {
+        guard state != .loading else { return }
+        state = .loading
+        let result = await cache.store(from: url, service: service)
+        state = result ? .ready : .error
+    }
+}
--- RedPaprika/ViewModel/MainViewModel.swift
+++ RedPaprika/ViewModel/MainViewModel.swift
@@ -2,3 +2,24 @@
 // Giorgio Ruscigno, 2025.
 
 import Foundation
+
+/// Holds the state of `MainView` depending on the results of the fetching of recipes.
+@MainActor
+final class MainViewModel: ObservableObject {
+    private let store: any RecipeStore
+    private let service: any RecipeService
+
+    @Published var state: ViewState = .idle
+
+    init(store: any RecipeStore, service: any RecipeService) {
+        self.store = store
+        self.service = service
+    }
+
+    func fetchRecipes() async {
+        guard state != .loading else { return }
+        state = .loading
+        let result = await store.fetchRecipes(service: service)
+        state = result ? .ready : .error
+    }
+}
