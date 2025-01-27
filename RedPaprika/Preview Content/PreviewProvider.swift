29a68c76d2a16ddbf1f2a4622067523a36f245b1
Gio <g.ruscigno@icloud.com>
2025-01-26 21:28:19 -0600

    update PreviewProvider, add an empty model container

--- RedPaprika/Preview Content/PreviewProvider.swift
+++ RedPaprika/Preview Content/PreviewProvider.swift
@@ -53,6 +53,22 @@ class PreviewProvider {
             fatalError("Failed to create model container for previewing: \(error.localizedDescription)")
         }
     }()
+
+    static let emptyPreviewContainer: ModelContainer = {
+        do {
+            let config = ModelConfiguration(isStoredInMemoryOnly: true)
+            let schema = Schema([
+                Cuisine.self,
+                Recipe.self,
+                CachedImage.self
+            ])
+            let container = try ModelContainer(for: schema, configurations: config)
+
+            return container
+        } catch {
+            fatalError("Failed to create model container for previewing: \(error.localizedDescription)")
+        }
+    }()
 }
 
 struct PreviewImageService: ImageService {
